7d66d7fd14fed3d427c67b2af87c2c9d9d2f3b0f
scottmeng <blackridermkz@gmail.com>
2014-11-05 23:06:40 +0800

    fix unicode problem

--- download_fb_msg.py
+++ download_fb_msg.py
@@ -4,19 +4,22 @@ import urllib
 import json
 from time import sleep
 
-QUERY_TEMPLATE = 'SELECT message_id, body, viewer_id, created_time, author_id , attachment FROM message WHERE thread_id = 484290431649855 ORDER BY created_time ASC LIMIT 30 OFFSET {0}'
+OUTPUT_DIR = 'fb_msg_by_lines2.dat'
+THREAD_ID = '484290431649855'
+ACCESS_TOKEN = 'CAACEdEose0cBABaRLvU5ZBBXzD7nR2uRraXcgp6D2lkGKZACnLavxynqmvAfDdjwIA2F6RRpXU7UpeyaOHwglQcWKejzpqzEL1C2DDhNuITLERX1UnlsEk0avxZCXq4stFzQn8FwwnjgnyxkHuIqdpmQFk3BZCZCw3SD72HtDLkRlz0qlrhpx6ipRDnmz7ZCJzgPfCiZB38RZCh5whfbd7pscds0PT27SMgZD'
+QUERY_TEMPLATE = 'SELECT message_id, body, viewer_id, created_time, author_id , attachment FROM message WHERE thread_id = {0} ORDER BY created_time ASC LIMIT 30 OFFSET {1}'
+
 
 def get_query(index):
-	return QUERY_TEMPLATE.format(index)
+	return QUERY_TEMPLATE.format(THREAD_ID, index)
 
 def get_url(index):
 	query = get_query(index);
-	params = urllib.urlencode({'q': query, 'access_token': 'CAADlGYxaLrEBAK8dHoMa1wdZCKdZCFhQMmN618WeZCRh7SzwZCIEuobnKCPsZCsaZALZA6CI6pbRwxbRpLcUYoWPZBTVdpiZCP2uGnSR0t8CKEVPbE1GZANT1XqqYmFZCxsZC7s4tdufDIIPLLxgvZCcbaCcNbzzRt43CmTMBYmX8ZAFLjoVxibP37W5ZCyS8uX7lgZAxbB3ZCnvW1IxXSqeKp78D793ZC'})
+	params = urllib.urlencode({'q': query, 'access_token': ACCESS_TOKEN})
 	url = 'https://graph.facebook.com/fql?' + params
 	return url
 
-
-f = open('fb_msg_by_lines.dat', 'w')
+f = open(OUTPUT_DIR, 'w')
 index = 0
 error_count = 0
 total_count = 0
@@ -24,13 +27,15 @@ while True:
 	url = get_url(index)
 	data = urllib.urlopen(url).read()
 	data = json.loads(data);
-	if ('data' in data):
+	if 'data' in data:
 		error_count = 0
 		for message in data['data']:
-			f.write(str(message))
+			f.write(json.dumps(message))
 			f.write('\n')
 		index += 30
 		total_count += len(data['data'])
+		if len(data['data']) = 0:
+			break
 		print '{0} / {1}'.format(total_count, index)
 		sleep(2)
 	else:
--- read_msg.py
+++ read_msg.py
@@ -1,8 +1,12 @@
-
 # -*- coding: utf-8 -*-
 
 import urllib
 import json
 
-with open('fb_msg_by_lines.dat', 'r') as f:
-	data = json.load(f)
+INPUT_DIR = 'fb_msg_by_lines2.dat'
+
+with open(INPUT_DIR, 'r') as f:
+	lines = f.readlines()
+	for line in lines:
+		line = json.loads(line)
+		print line['body']
